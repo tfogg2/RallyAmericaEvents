41eee266a97841c6ee70efd573ae3a1548746020
tfogg2 <foggts@g.cofc.edu>
2017-09-05 16:38:58 -0400

    login forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
   ActiveAdmin.routes(self)
   root to: 'home#index'
   resources :teams
+  devise_scope :user do
+   get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+   post '/sign_up' => 'devise/registrations#create'
+  end
+
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   resources :volunteers
   resources :events do
