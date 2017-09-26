c6b1831617a29e636426e292dd89ebb5d3ebdc7b
tfogg2 <foggts@g.cofc.edu>
2017-09-26 13:14:44 -0400

    logo width

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -21,7 +21,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs do
       f.input :email
-      if(current_user.is_super_admin?)
+      if(current_user.is_super_admin? || f.object == current_user)
         f.input :password
         f.input :password_confirmation
       end
