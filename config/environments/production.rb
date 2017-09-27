422397bf351bd48f761330059171e4836b0d7d54
tfogg2 <foggts@g.cofc.edu>
2017-09-27 13:59:32 -0400

    fog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,16 +31,16 @@ Rails.application.configure do
   config.assets.compile = true
 
 
-  # config.paperclip_defaults = {
-  #   storage: :s3,
-  #   s3_credentials: {
-  #     :preserve_files => true,
-  #     bucket: ENV.fetch('S3_BUCKET_NAME'),
-  #     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-  #     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-  #     s3_region: ENV.fetch('AWS_REGION')
-  #   }
-  # }
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      :preserve_files => true,
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION')
+    }
+  }
 
   connection = Fog::Storage.new({
     :provider                 => 'AWS',
