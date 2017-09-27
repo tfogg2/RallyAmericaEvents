699b65cab0b5b1a59be79be79a533a7784ffa1ba
tfogg2 <foggts@g.cofc.edu>
2017-09-27 14:17:52 -0400

    fog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
     storage: :s3,
     s3_credentials: {
       :preserve_files => true,
+      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
