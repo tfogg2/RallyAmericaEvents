7c19ccdaee06f600c7b0d4dbfc2e240d8a5ae9e2
tfogg2 <foggts@g.cofc.edu>
2017-09-27 14:36:47 -0400

    fog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,9 +36,10 @@ Rails.application.configure do
     s3_credentials: {
       :preserve_files => true,
       bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: 'AKIAIIB5WGLM5QNUQUBQ',
-      secret_access_key: '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',
-      s3_region: ENV.fetch('AWS_REGION')
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
     }
   }
 
