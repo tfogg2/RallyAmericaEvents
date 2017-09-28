a8b2ee0c43edf7083d077e0c7e7d953d8f673ee8
tfogg2 <foggts@g.cofc.edu>
2017-09-28 11:49:00 -0400

    fog rally bucket

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,16 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :registerable
   has_many :volunteers
-  has_attached_file :avatar #,
-  # :styles => {
-  #     :thumbnail => '100x100>',
-  #     :regular => '140x200>'
-  #   },
-  # :storage => :s3,
-  #   :s3_credentials => {
-  #     :bucket => ENV['AWS_BUCKET'],
-  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #   },
-  #   :path => ":filename.:extension"
+  has_attached_file :avatar,
+  :storage => :s3,
+    :s3_credentials => {
+      :preserve_files => true,
+      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION')
+    }
 
   validates_attachment_content_type :avatar, content_type: ['image/jpeg', 'image/png', 'image/gif']
 
